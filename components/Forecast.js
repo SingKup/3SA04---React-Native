35b7611820c76795553ffb42e5a61f5cc9052445
SingKup <thanawat452@gmail.com>
2020-09-18 11:59:39 +0700

    Update

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,29 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function Forecast(props) {
     return (
         <View style={styles.center}>
             <Text style={styles.big}>{props.main}</Text>
-            <Text>{props.description}</Text>
-            <View>
-                <Text style={styles.big}>{props.temp}</Text>
-                <Text style={styles.medium}>°C</Text>
+            <Text style={styles.medium}>{props.description}</Text>
+            <View style={{marginTop: 20}}>
+                <Text>
+                    <Text style={styles.big}>{props.temp}  </Text>
+                    <Text style={styles.medium}>°C</Text>
+                </Text>
             </View>
         </View>
     );
 }
+
 const styles = StyleSheet.create({
-    center: {
-        justifyContent: 'center' ,
-        alignItems: 'center' ,
-        
-    }
+    center: {        
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
     big: {
+        marginTop: 20,
         fontSize: 30,
         color: 'white',
     },
     medium: {
+        marginTop: 20,
         fontSize: 15,
         color: 'white',
     }
